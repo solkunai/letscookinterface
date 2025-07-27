8d9369257ef3910baae2ba93c20defd85489fb27
Kunai Vlogs <kunaivlogsdaily@gmail.com>
2025-07-27 23:42:53 +0000

    for the main headline, try abril face font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ["'PT Sans'", 'sans-serif'],
-        headline: ["'Playfair Display'", 'serif'],
+        headline: ["'Abril Fatface'", 'serif'],
         code: ['monospace'],
       },
       colors: {
