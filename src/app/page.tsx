397ac5bb69144c42cb769a20f5b7cded157d60b0
Kunai Vlogs <kunaivlogsdaily@gmail.com>
2025-07-27 23:17:48 +0000

    also not the correct x logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,15 @@ import { cn } from '@/lib/utils';
 import { LoadingDots } from '@/components/loading-dots';
 
 const XIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg 
-    xmlns="http://www.w3.org/2000/svg" 
-    width="24" 
-    height="24" 
-    viewBox="0 0 24 24" 
-    fill="none" 
-    stroke="currentColor" 
-    strokeWidth="2" 
-    strokeLinecap="round" 
-    strokeLinejoin="round" 
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="currentColor"
     {...props}
   >
-    <path d="M18 6 6 18" />
-    <path d="m6 6 12 12" />
+    <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
   </svg>
 );
 
