1588c60dc624d98330cc3c3d8892e87ef5390db6
Kunai Vlogs <kunaivlogsdaily@gmail.com>
2025-08-05 17:01:34 +0000

    I got this too "npm warn deprecated inflight@1.0.6: This module is not s

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,13 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        '@/*': './src/*',
+      },
+    },
+  },
 };
 
 export default nextConfig;
